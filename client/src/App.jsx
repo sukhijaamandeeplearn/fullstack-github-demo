ade26c716daa25495a4a13b242f656d2ae7edb30
sukhijaamandeeplearn <sukhija.amandeep.learn@gmail.com>
2025-08-05 13:40:35 +0100

    feat: update App.jsx in line-a

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,8 @@ function App() {
   useEffect(() => {
     fetch('http://localhost:3001/api/hello')
       .then((res) => res.json())
-      .then((data) => setMsg(data.message));
+      //.then((data) => setMsg(data.message));
+        .then((data) => setMsg("Change A from line-a"));
   }, []);
 
   return (
